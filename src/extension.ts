e9874eba441c25483a43d8615c5338a5fbc84274
budi-imam-prasetyo <111475648+budi-imam-prasetyo@users.noreply.github.com>
2024-07-06 10:06:58 +0700

    first commit again

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,16 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.devServe', () => {
-        // Create and show a new terminal
-        const terminal1 = vscode.window.createTerminal('Dev Server');
-        terminal1.sendText('npm run dev');
 
-        // Create and show another new terminal
-        const terminal2 = vscode.window.createTerminal('Serve Server');
-        terminal2.sendText('npm run serve');
+    let disposable = vscode.commands.registerCommand('extension.ryoDevServe', () => {
+        const terminalDev = vscode.window.createTerminal({ name: "Dev Terminal" });
+        terminalDev.sendText("npm run dev");
 
-        // Show terminals
-        terminal1.show();
-        terminal2.show();
+        const terminalServe = vscode.window.createTerminal({ name: "Serve Terminal" });
+        terminalServe.sendText("npm run serve");
+
+        terminalDev.show();
+        terminalServe.show();
     });
 
     context.subscriptions.push(disposable);
